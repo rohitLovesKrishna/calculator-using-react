0df9acff431b69f153371fc966c6847f255e685a
rohitLovesKrishna <ambawata.11@gmail.com>
2023-02-05 14:35:29 +0530

    Done

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -2,6 +2,6 @@ import React from 'react'
 import styles from './Button.module.css'
 export default function Button(props) {
   return (
-      <button onClick={props.onClik} className={`${styles.cstmBtn} ${props.className}`} type='button'>{props.children}</button>
+      <button onClick={props.onClick} id={props.id} value={props.value} className={`${styles.cstmBtn} ${props.className}`} type='button'>{props.children}</button>
   )
 }
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -7,32 +7,50 @@ import { MdScreenRotation } from "react-icons/md";
 import styles from './Calculator.module.css'
 import { Button} from '../'
 
-export default function Calculator() {
-const [value,setValue] = useState('0')
+export default function Calculator(props) {
+const [Value,setValue] = useState('0')
+    const sumHandler = (value)=>{
+    setValue(eval(value))
+    }
+    const removeLast = ()=>{
+        if (Value.length > 1 && Value !== '0') {
+            setValue((prev) =>
+                prev.substring(0, prev.length - 1)
+            )}
+            else if(Value.length > 0 && Value !== 0){
+setValue('0');
+            }
+        }
+    const val = (e)=>{
+        if(Value === '0' && Value.length < 2){setValue('')}
+        if(e.target.id === undefined){//Do Nothing
+}else{setValue((prev) => prev += e.target.id)}
+    }
+
     return (
         <div className={styles.body__border}>
             <div className={styles.calculator__border}>
-                <input type='text' value={value} className={styles.screenBox}/>
-                <Button><AiOutlineClear/></Button>
-                <Button><BsBackspace/></Button>
-                <Button><FaPercent/></Button>
-                <Button><FaDivide/></Button>
-                <Button>7</Button>
-                <Button>8</Button>
-                <Button>9</Button>
-                <Button><ImCross/></Button>
-                <Button>4</Button>
-                <Button>5</Button>
-                <Button>6</Button>
-                <Button><AiOutlineMinus/></Button>
-                <Button>1</Button>
-                <Button>2</Button>
-                <Button>3</Button>
-                <Button><AiOutlinePlus/></Button>
-                <Button><MdScreenRotation/></Button>
-                <Button>0</Button>
-                <Button><BsDot/></Button>
-                <Button><FaEquals/></Button>
+                <div  className={styles.screenBox}>{Value}</div>
+                <Button onClick={()=>{setValue('0')}}><AiOutlineClear/></Button>
+                <Button  onClick={(s) => removeLast()}><BsBackspace/></Button>
+                <Button id={'%'} onClick={(e) => val(e)}><FaPercent/></Button>
+                <Button id={'/'} onClick={(e) => val(e)}><FaDivide/></Button>
+                <Button id={7} onClick={(e)=>val(e)}>7</Button>
+                <Button id={8} onClick={(e) => val(e)}>8</Button>
+                <Button id={9} onClick={(e) => val(e)}>9</Button>
+                <Button id={'*'} onClick={(e) => val(e)}><ImCross/></Button>
+                <Button id={4} onClick={(e) => val(e)}>4</Button>
+                <Button id={5} onClick={(e) => val(e)}>5</Button>
+                <Button id={6} onClick={(e) => val(e)}>6</Button>
+                <Button id={'-'} onClick={(e) => val(e)}><AiOutlineMinus/></Button>
+                <Button id={1} onClick={(e) => val(e)}>1</Button>
+                <Button id={2} onClick={(e) => val(e)}>2</Button>
+                <Button id={3} onClick={(e) => val(e)}>3</Button>
+                <Button id={'+'} onClick={(e) => val(e)}><AiOutlinePlus/></Button>
+                <Button value='Orientation' onClick={(e) => val(e)}><MdScreenRotation/></Button>
+                <Button id={0} onClick={(e) => val(e)}>0</Button>
+                <Button id={'.'} onClick={(e) => val(e)}><BsDot/></Button>
+                <Button onClick={()=>sumHandler(Value)}><FaEquals/></Button>
             </div>
         </div>
     )
